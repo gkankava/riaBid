7d1d569fb67abfd325db0ff4f76d2fbcd8e80e84
gordela <gordogordel@gmail.com>
2021-08-04 07:55:41 +0400

    S

--- src/containers/historyDashboard.js
+++ src/containers/historyDashboard.js
@@ -14,6 +14,7 @@ import Loading from "./loading";
 import { getJwt } from "../services/authService";
 import jwt_decode from "jwt-decode";
 import { MetaTags } from "react-meta-tags";
+
 function HistoryDashboard(props) {
   const [filter, setFilter] = React.useState(false);
   var { user_id } = jwt_decode(getJwt());
